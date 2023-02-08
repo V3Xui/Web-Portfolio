1a3f2ed8aa7e678a2da83190558158edf7e62e59
Melvin Dolot <112602081+V3Xui@users.noreply.github.com>
2023-02-09 00:40:06 +0800

    Delete main.js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-// Typing Animation
-let typed = new Typed(".typing", {
-    strings:["", "Hungry Learner", "Gamer",  "Manhwa Reader", "Future Web Developer"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
