68b5b4430af495e79ecb1fd6eddb579da354d6ef
Melvin Dolot <112602081+V3Xui@users.noreply.github.com>
2023-02-10 17:09:54 +0800

    Update typing.js

--- js/typing.js
+++ js/typing.js
@@ -1,6 +1,6 @@
 // Typing Animation
 let typed = new Typed(".typing", {
-    strings:["", "Hungry Learner", "Gamer",  "Manhwa Reader", "Future Web Developer"],
+    strings:["", "Hungry Learner", "Gamer",  "Manhwa Reader", "Future Web Developer", "Aspiring Software Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
