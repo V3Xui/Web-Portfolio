717cb30d5d84053e3b4f7dc6f7125cde45af32c8
Melvin Dolot <112602081+V3Xui@users.noreply.github.com>
2023-03-09 13:31:21 +0800

    Update typing.js
    More fun

--- js/typing.js
+++ js/typing.js
@@ -1,7 +1,60 @@
+const menuBtn = document.querySelector(".menuBtn");
+const navMenu = document.querySelector(".nav-menu");
+
+menuBtn.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  menuBtn.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
 // Typing Animation
 let typed = new Typed(".typing", {
-    strings:["", "Hungry Learner", "Gamer",  "Manhwa Reader", "Future Web Developer", "Aspiring Software Developer"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
+  strings: [
+    "",
+    "a Hungry Learner",
+    "An Aspiring Web Developer",
+    "Adaptable",
+    "a Manhwa Reader",
+    "An Aspiring Software Developer",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
+
+let modal1 = document.getElementById("modal1");
+let button1 = document.getElementById("modal-button1");
+let span1 = document.getElementsByClassName("close")[0];
+
+button1.onclick = function () {
+  modal1.style.display = "block";
+};
+
+span1.onclick = function () {
+  modal1.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+};
+
+let modal2 = document.getElementById("modal2");
+let button2 = document.getElementById("modal-button2");
+let span2 = document.getElementsByClassName("close")[1];
+
+button2.onclick = function () {
+  modal2.style.display = "block";
+};
+
+span2.onclick = function () {
+  modal2.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+};
