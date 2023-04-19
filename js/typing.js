9e25841266c242174bde54dfc33598d4665a1e03
Melvin Dolot <112602081+V3Xui@users.noreply.github.com>
2023-04-19 19:37:04 +0800

    Update typing.js

--- js/typing.js
+++ js/typing.js
@@ -58,3 +58,40 @@ window.onclick = function (event) {
     modal2.style.display = "none";
   }
 };
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document
+    .getElementsByClassName("slides")[0]
+    .getElementsByTagName("img");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+let prev = document.getElementsByClassName("prev")[0];
+let next = document.getElementsByClassName("next")[0];
+prev.addEventListener("click", function () {
+  plusSlides(-1);
+});
+next.addEventListener("click", function () {
+  plusSlides(1);
+});
